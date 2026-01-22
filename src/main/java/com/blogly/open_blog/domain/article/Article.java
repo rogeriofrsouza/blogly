d00fc9cee2123bb5011e1585110edf238d408bdf
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2026-01-21 23:24:23 -0300

    feat(domain): add Article entity and migration

---
+++ src/main/java/com/blogly/open_blog/domain/article/Article.java
@@ -0,0 +1,29 @@
+package com.blogly.open_blog.domain.article;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "article",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uc_article_title", columnNames = {"title"})
+        })
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Lob
+    @Column(nullable = false)
+    private String body;
+
+    @Column(length = 100)
+    private String summary;
+}
