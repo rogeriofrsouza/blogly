0de9adcf64a48e52df2720d1c8035d7fb3d5a73e
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2026-04-11 15:19:36 -0300

    feat(user): add user jpa entity and flyway migration
    feat(user): add user jpa entity and flyway migration

---
+++ src/main/java/com/blogly/blogly/infrastructure/persistence/UserEntity.java
@@ -0,0 +1,27 @@
+package com.blogly.blogly.infrastructure.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 150)
+    private String password;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+}
---
+++ src/main/resources/db/migration/V2__add_user_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "user"
+(
+    id       bigint PRIMARY KEY,
+    email    varchar(50)  NOT NULL UNIQUE,
+    password varchar(150) NOT NULL,
+    name     varchar(100) NOT NULL
+);
