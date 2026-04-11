e8245572ea764f6e85047dbbaa58886bc9d678aa
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2026-04-11 15:01:31 -0300

    feat(config): add PasswordEncoder bean

---
+++ src/main/java/com/blogly/blogly/config/AuthConfiguration.java
@@ -0,0 +1,15 @@
+package com.blogly.blogly.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+class AuthConfiguration {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
