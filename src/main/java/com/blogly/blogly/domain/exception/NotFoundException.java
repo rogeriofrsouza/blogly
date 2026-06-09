f02c7cdfd9e0366789da26cf8cc345411e168ac1
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2026-06-08 22:11:33 -0300

    refactor(exception): domain convert from Java to Kotlin

--- src/main/java/com/blogly/blogly/domain/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.blogly.blogly.domain.exception;
-
-public abstract class NotFoundException extends DomainException {
-
-    protected NotFoundException(Class<?> clazz, Long id) {
-        super("%s not found with id: %d".formatted(clazz.getSimpleName(), id));
-    }
-}
