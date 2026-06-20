eeb67446f22cd059bae1969097a2a8b1ab029f38
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2026-06-19 21:29:37 -0300

    refactor(auth): presentation convert from Java to Kotlin

---
+++ src/main/java/com/blogly/blogly/presentation/auth/AuthController.kt
@@ -0,0 +1,38 @@
+package com.blogly.blogly.presentation.auth
+
+import com.blogly.blogly.application.auth.SignInUseCase
+import com.blogly.blogly.application.auth.SignUpUseCase
+import com.blogly.blogly.application.auth.dto.SignInResponse
+import com.blogly.blogly.application.auth.dto.SignUpResponse
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+
+@RequestMapping("/auth")
+@RestController
+class AuthController(
+    private val signUpUseCase: SignUpUseCase,
+    private val signInUseCase: SignInUseCase
+) {
+    @PostMapping("/signup")
+    fun signUp(@Valid @RequestBody dto: SignUpRequestDto): ResponseEntity<SignUpResponse> {
+        val response = signUpUseCase.execute(dto.toRequest())
+
+        val location = ServletUriComponentsBuilder.fromCurrentContextPath()
+            .path("/api/users/{id}")
+            .buildAndExpand(response.id)
+            .toUri()
+
+        return ResponseEntity.created(location).body(response)
+    }
+
+    @PostMapping("/signin")
+    fun signIn(@Valid @RequestBody dto: SignInRequestDto): ResponseEntity<SignInResponse> {
+        val response = signInUseCase.execute(dto.toRequest())
+        return ResponseEntity.ok(response)
+    }
+}
