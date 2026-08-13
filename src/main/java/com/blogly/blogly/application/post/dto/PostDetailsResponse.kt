7b73d0d4fb2bacbf0d1ed0137ae9cd4ece0224e6
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2026-08-13 13:04:04 -0300

    feat(post): add creation time

--- src/main/java/com/blogly/blogly/application/post/dto/PostDetailsResponse.kt
+++ src/main/java/com/blogly/blogly/application/post/dto/PostDetailsResponse.kt
@@ -2,18 +2,22 @@ package com.blogly.blogly.application.post.dto
 
 import com.blogly.blogly.application.shared.TsidCodec
 import com.blogly.blogly.domain.post.Post
+import java.time.Instant
+import kotlin.time.toJavaInstant
 
 data class PostDetailsResponse(
     val id: String,
     val title: String,
-    val content: String
+    val content: String,
+    val createdAt: Instant,
 ) {
     companion object {
         fun from(post: Post) =
             PostDetailsResponse(
                 id = TsidCodec.encode(post.id.value),
                 title = post.title.value,
-                content = post.content.value
+                content = post.content.value,
+                createdAt = post.createdAt.toJavaInstant(),
             )
     }
 }
