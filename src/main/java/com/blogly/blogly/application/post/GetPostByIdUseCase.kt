6a2f702dd328f9075b50236289c892b9a437eaf3
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2026-08-25 15:42:20 -0300

    feat(post): update get by id usecase with new visibility check

--- src/main/java/com/blogly/blogly/application/post/GetPostByIdUseCase.kt
+++ src/main/java/com/blogly/blogly/application/post/GetPostByIdUseCase.kt
@@ -2,7 +2,7 @@ package com.blogly.blogly.application.post
 
 import com.blogly.blogly.application.post.dto.PostDetailsResponse
 import com.blogly.blogly.application.post.dto.toDetailsResponse
-import com.blogly.blogly.domain.post.Post
+import com.blogly.blogly.application.shared.UserProvider
 import com.blogly.blogly.domain.post.PostId
 import com.blogly.blogly.domain.post.PostRepository
 import com.blogly.blogly.domain.post.exception.PostNotFoundException
@@ -10,10 +10,16 @@ import org.springframework.stereotype.Component
 
 @Component
 class GetPostByIdUseCase(
-    private val repository: PostRepository
+    private val repository: PostRepository,
+    private val userProvider: UserProvider
 ) {
-    fun execute(postId: PostId): PostDetailsResponse =
-        repository.findById(postId)
-            ?.let(Post::toDetailsResponse)
-            ?: throw PostNotFoundException(postId)
+    fun execute(postId: PostId): PostDetailsResponse {
+        val post = repository.findById(postId) ?: throw PostNotFoundException(postId)
+
+        if (!post.isVisibleTo(userProvider.currentUserOrNull()?.id)) {
+            throw PostNotFoundException(postId)
+        }
+
+        return post.toDetailsResponse()
+    }
 }
