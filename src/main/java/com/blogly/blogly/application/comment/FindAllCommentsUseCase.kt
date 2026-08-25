c097c348dcbe8f1fe66dd45e6a31c01c3cf4c11c
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2026-08-25 15:59:49 -0300

    feat: update usecases with new visibility guard

--- src/main/java/com/blogly/blogly/application/comment/FindAllCommentsUseCase.kt
+++ src/main/java/com/blogly/blogly/application/comment/FindAllCommentsUseCase.kt
@@ -2,20 +2,19 @@ package com.blogly.blogly.application.comment
 
 import com.blogly.blogly.application.comment.dto.CommentDetailsResponse
 import com.blogly.blogly.application.comment.dto.toDetailsResponse
+import com.blogly.blogly.application.post.PostVisibilityGuard
 import com.blogly.blogly.domain.comment.Comment
 import com.blogly.blogly.domain.comment.CommentRepository
 import com.blogly.blogly.domain.post.PostId
-import com.blogly.blogly.domain.post.PostRepository
-import com.blogly.blogly.domain.post.exception.PostNotFoundException
 import org.springframework.stereotype.Component
 
 @Component
 class FindAllCommentsUseCase(
     private val repository: CommentRepository,
-    private val postRepository: PostRepository
+    private val visibilityGuard: PostVisibilityGuard
 ) {
     fun execute(postId: PostId): List<CommentDetailsResponse> {
-        postRepository.findById(postId) ?: throw PostNotFoundException(postId)
+        visibilityGuard.resolveVisiblePost(postId)
 
         return repository.findByPublishedPostId(postId)
             .map(Comment::toDetailsResponse)
--- src/main/java/com/blogly/blogly/application/post/GetPostByIdUseCase.kt
+++ src/main/java/com/blogly/blogly/application/post/GetPostByIdUseCase.kt
@@ -2,24 +2,14 @@ package com.blogly.blogly.application.post
 
 import com.blogly.blogly.application.post.dto.PostDetailsResponse
 import com.blogly.blogly.application.post.dto.toDetailsResponse
-import com.blogly.blogly.application.shared.UserProvider
 import com.blogly.blogly.domain.post.PostId
-import com.blogly.blogly.domain.post.PostRepository
-import com.blogly.blogly.domain.post.exception.PostNotFoundException
 import org.springframework.stereotype.Component
 
 @Component
 class GetPostByIdUseCase(
-    private val repository: PostRepository,
-    private val userProvider: UserProvider
+    private val visibilityGuard: PostVisibilityGuard
 ) {
-    fun execute(postId: PostId): PostDetailsResponse {
-        val post = repository.findById(postId) ?: throw PostNotFoundException(postId)
-
-        if (!post.isVisibleTo(userProvider.currentUserOrNull()?.id)) {
-            throw PostNotFoundException(postId)
-        }
-
-        return post.toDetailsResponse()
-    }
+    fun execute(postId: PostId): PostDetailsResponse =
+        visibilityGuard.resolveVisiblePost(postId)
+            .toDetailsResponse()
 }
