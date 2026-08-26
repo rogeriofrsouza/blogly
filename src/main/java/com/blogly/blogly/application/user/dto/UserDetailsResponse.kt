0a747eb5217d7ec3c48cea80167a617e73f35d29
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2026-08-25 21:37:33 -0300

    feat(user): add new fields to response DTO

--- src/main/java/com/blogly/blogly/application/user/dto/UserDetailsResponse.kt
+++ src/main/java/com/blogly/blogly/application/user/dto/UserDetailsResponse.kt
@@ -2,12 +2,17 @@ package com.blogly.blogly.application.user.dto
 
 import com.blogly.blogly.application.shared.TsidCodec
 import com.blogly.blogly.domain.user.User
+import java.time.Instant
+import kotlin.time.toJavaInstant
 
 data class UserDetailsResponse(
     val id: String,
     val email: String,
     val role: String,
-    val name: String
+    val name: String,
+    val bio: String?,
+    val avatarKey: String?,
+    val joinedAt: Instant
 )
 
 fun User.toDetailsResponse() =
@@ -15,5 +20,8 @@ fun User.toDetailsResponse() =
         id = TsidCodec.encode(id.value),
         email = email.value,
         role = role.name,
-        name = name.value
+        name = name.value,
+        bio = bio?.value,
+        avatarKey = avatarKey?.value,
+        joinedAt = joinedAt.toJavaInstant()
     )
