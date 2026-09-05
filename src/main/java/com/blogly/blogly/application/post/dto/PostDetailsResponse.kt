c9e1cbce2dae468e82194517cd2b34ec00698caa
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2026-09-05 15:04:57 -0300

    feat(post): add userId to response DTO

--- src/main/java/com/blogly/blogly/application/post/dto/PostDetailsResponse.kt
+++ src/main/java/com/blogly/blogly/application/post/dto/PostDetailsResponse.kt
@@ -6,12 +6,14 @@ import com.blogly.blogly.domain.post.Post
 data class PostDetailsResponse(
     val id: String,
     val title: String,
-    val content: String
+    val content: String,
+    val userId: String
 )
 
 fun Post.toDetailsResponse() =
     PostDetailsResponse(
         id = TsidCodec.encode(id.value),
         title = title.value,
-        content = content.value
+        content = content.value,
+        userId = TsidCodec.encode(userId.value)
     )
