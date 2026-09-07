901da176332d9d8523cb02fb8f8163b1f62e5f58
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2026-09-06 23:03:08 -0300

    feat(comment): remove guard and inline domain checks into use cases

--- src/main/java/com/blogly/blogly/application/comment/DeleteCommentUseCase.kt
+++ src/main/java/com/blogly/blogly/application/comment/DeleteCommentUseCase.kt
@@ -1,16 +1,22 @@
 package com.blogly.blogly.application.comment
 
+import com.blogly.blogly.application.shared.UserProvider
 import com.blogly.blogly.domain.comment.CommentId
+import com.blogly.blogly.domain.comment.CommentNotFoundException
+import com.blogly.blogly.domain.comment.CommentNotOwnedException
 import com.blogly.blogly.domain.comment.CommentRepository
+import com.blogly.blogly.domain.shared.domainCheck
 import org.springframework.stereotype.Component
 
 @Component
 class DeleteCommentUseCase(
     private val repository: CommentRepository,
-    private val accessGuard: CommentAccessGuard
+    private val userProvider: UserProvider
 ) {
     fun execute(commentId: CommentId) {
-        val comment = accessGuard.resolveOwnedComment(commentId)
+        val comment = repository.findById(commentId) ?: throw CommentNotFoundException(commentId)
+        domainCheck(comment.isAuthoredBy(userProvider.currentUserId())) { CommentNotOwnedException(commentId) }
+
         repository.deleteById(comment.id)
     }
 }
--- src/main/java/com/blogly/blogly/application/comment/UpdateCommentUseCase.kt
+++ src/main/java/com/blogly/blogly/application/comment/UpdateCommentUseCase.kt
@@ -1,20 +1,28 @@
 package com.blogly.blogly.application.comment
 
 import com.blogly.blogly.application.comment.dto.UpdateCommentRequest
-import com.blogly.blogly.domain.comment.CommentBody
-import com.blogly.blogly.domain.comment.CommentId
-import com.blogly.blogly.domain.comment.CommentRepository
+import com.blogly.blogly.application.shared.UserProvider
+import com.blogly.blogly.domain.comment.*
+import com.blogly.blogly.domain.post.PostRepository
+import com.blogly.blogly.domain.post.exception.PostNotCommentableException
+import com.blogly.blogly.domain.post.exception.PostNotFoundException
+import com.blogly.blogly.domain.shared.domainCheck
 import org.springframework.stereotype.Component
 
 @Component
 class UpdateCommentUseCase(
     private val repository: CommentRepository,
-    private val accessGuard: CommentAccessGuard
+    private val postRepository: PostRepository,
+    private val userProvider: UserProvider
 ) {
     fun execute(commentId: CommentId, request: UpdateCommentRequest) {
-        val comment = accessGuard.resolveOwnedComment(commentId)
-        comment.update(CommentBody(request.body))
+        val comment = repository.findById(commentId) ?: throw CommentNotFoundException(commentId)
+        val post = postRepository.findById(comment.postId) ?: throw PostNotFoundException(comment.postId)
 
+        domainCheck(post.canBeCommentedOn()) { PostNotCommentableException(post.id, post.status) }
+        domainCheck(comment.isAuthoredBy(userProvider.currentUserId())) { CommentNotOwnedException(commentId) }
+
+        comment.update(CommentBody(request.body))
         repository.save(comment)
     }
 }
