0384338f1742721a195df4fe8a9bd1a4579370a2
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2026-09-07 10:27:48 -0300

    feat(post): remove guard and inline domain checks into use cases

--- src/main/java/com/blogly/blogly/application/comment/FindAllPostCommentsUseCase.kt
+++ src/main/java/com/blogly/blogly/application/comment/FindAllPostCommentsUseCase.kt
@@ -2,19 +2,24 @@ package com.blogly.blogly.application.comment
 
 import com.blogly.blogly.application.comment.dto.CommentDetailsResponse
 import com.blogly.blogly.application.comment.dto.toDetailsResponse
-import com.blogly.blogly.application.post.PostVisibilityGuard
+import com.blogly.blogly.application.shared.UserProvider
 import com.blogly.blogly.domain.comment.Comment
 import com.blogly.blogly.domain.comment.CommentRepository
 import com.blogly.blogly.domain.post.PostId
+import com.blogly.blogly.domain.post.PostRepository
+import com.blogly.blogly.domain.post.exception.PostNotFoundException
+import com.blogly.blogly.domain.shared.domainCheck
 import org.springframework.stereotype.Component
 
 @Component
 class FindAllPostCommentsUseCase(
     private val repository: CommentRepository,
-    private val visibilityGuard: PostVisibilityGuard
+    private val postRepository: PostRepository,
+    private val userProvider: UserProvider
 ) {
     fun execute(postId: PostId): List<CommentDetailsResponse> {
-        visibilityGuard.resolveVisiblePost(postId)
+        val post = postRepository.findById(postId) ?: throw PostNotFoundException(postId)
+        domainCheck(post.isVisibleTo(userProvider.currentUserIdOrNull())) { PostNotFoundException(postId) }
 
         return repository.findByPublishedPostId(postId)
             .map(Comment::toDetailsResponse)
--- src/main/java/com/blogly/blogly/application/post/GetPostByIdUseCase.kt
+++ src/main/java/com/blogly/blogly/application/post/GetPostByIdUseCase.kt
@@ -2,14 +2,22 @@ package com.blogly.blogly.application.post
 
 import com.blogly.blogly.application.post.dto.PostDetailsResponse
 import com.blogly.blogly.application.post.dto.toDetailsResponse
+import com.blogly.blogly.application.shared.UserProvider
 import com.blogly.blogly.domain.post.PostId
+import com.blogly.blogly.domain.post.PostRepository
+import com.blogly.blogly.domain.post.exception.PostNotFoundException
+import com.blogly.blogly.domain.shared.domainCheck
 import org.springframework.stereotype.Component
 
 @Component
 class GetPostByIdUseCase(
-    private val visibilityGuard: PostVisibilityGuard
+    private val repository: PostRepository,
+    private val userProvider: UserProvider
 ) {
-    fun execute(postId: PostId): PostDetailsResponse =
-        visibilityGuard.resolveVisiblePost(postId)
-            .toDetailsResponse()
+    fun execute(postId: PostId): PostDetailsResponse {
+        val post = repository.findById(postId) ?: throw PostNotFoundException(postId)
+        domainCheck(post.isVisibleTo(userProvider.currentUserIdOrNull())) { PostNotFoundException(postId) }
+
+        return post.toDetailsResponse()
+    }
 }
